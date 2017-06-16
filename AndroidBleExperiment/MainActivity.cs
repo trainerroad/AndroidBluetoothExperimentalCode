cc4087aa022c7fb2abc1c5888092dceff8bbe0ed
Frank Schwieterman <frank@trainerroad.com>
2017-06-16 13:07:37 -0500

    try to connect to a fixed the device (Wahoo test's kickr)

--- AndroidBleExperiment/MainActivity.cs
+++ AndroidBleExperiment/MainActivity.cs
@@ -41,18 +41,18 @@ namespace AndroidBleExperiment
         {
             base.OnScanFailed(errorCode);
 
-            Debug.WriteLine("OnScanFailed: " + errorCode);
+            Debug.WriteLine("ScanCallback::OnScanFailed: " + errorCode);
         }
 
         public override void OnBatchScanResults(IList<ScanResult> results)
         {
             base.OnBatchScanResults(results);
 
-            Debug.WriteLine("OnBatchScanResults:");
+            Debug.WriteLine("ScanCallback::OnBatchScanResults:");
 
             foreach (var scanResult in results)
             {
-                TraceScanResult(scanResult);
+                HandleScanResult(scanResult);
             }
         }
 
@@ -62,12 +62,35 @@ namespace AndroidBleExperiment
 
             Debug.WriteLine($"OnScanResult ({callbackType}):");
 
-            TraceScanResult(scanResult);
+            HandleScanResult(scanResult);
         }
 
-        private void TraceScanResult(ScanResult scanResult)
+        private bool _triedConnect = false;
+        private string _connectAddressTarget = "CA:99:61:99:B2:DA";
+        private string _connectDeviceNameTarget = "Wahoo KICKR F284";
+
+        private void HandleScanResult(ScanResult scanResult)
         {
             Debug.WriteLine($"    {scanResult.Device.Address}, DeviceName:{scanResult.Device.Name}, ScanRecord:{scanResult.ScanRecord.DeviceName}");
+
+            if (!_triedConnect && scanResult.Device.Address == _connectAddressTarget &&
+                scanResult.ScanRecord.DeviceName == _connectDeviceNameTarget)
+            {
+                _triedConnect = true;
+
+                Debug.WriteLine($"    Attempting connect....");
+                scanResult.Device.ConnectGatt(null, false, new SimpleGattCallback());
+            }
+        }
+    }
+
+    public class SimpleGattCallback : BluetoothGattCallback
+    {
+        public override void OnConnectionStateChange(BluetoothGatt gatt, GattStatus status, ProfileState newState)
+        {
+            base.OnConnectionStateChange(gatt, status, newState);
+
+            Debug.WriteLine($"BluetoothGattCallback::OnConnectionStateChange: {gatt.Device.Address}, {status}, {newState}");
         }
     }
 }
