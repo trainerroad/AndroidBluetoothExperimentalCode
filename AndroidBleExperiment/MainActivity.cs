6cd19d11aaebca74b091c8d7ec72ae5565e47b5e
Frank Schwieterman <frank@trainerroad.com>
2017-06-16 12:54:41 -0500

    add minimal BLE scanning

--- AndroidBleExperiment/MainActivity.cs
+++ AndroidBleExperiment/MainActivity.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using Android.App;
+using Android.Bluetooth;
+using Android.Bluetooth.LE;
 using Android.Content;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+
+using Debug = System.Diagnostics.Debug;
 
 namespace AndroidBleExperiment
 {
@@ -25,6 +30,44 @@ namespace AndroidBleExperiment
             Button button = FindViewById<Button>(Resource.Id.MyButton);
 
             button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+            BluetoothAdapter.DefaultAdapter.BluetoothLeScanner.StartScan(new SimpleScanCallback());
+        }
+    }
+
+    public class SimpleScanCallback : ScanCallback
+    {
+        public override void OnScanFailed(ScanFailure errorCode)
+        {
+            base.OnScanFailed(errorCode);
+
+            Debug.WriteLine("OnScanFailed: " + errorCode);
+        }
+
+        public override void OnBatchScanResults(IList<ScanResult> results)
+        {
+            base.OnBatchScanResults(results);
+
+            Debug.WriteLine("OnBatchScanResults:");
+
+            foreach (var scanResult in results)
+            {
+                TraceScanResult(scanResult);
+            }
+        }
+
+        public override void OnScanResult(ScanCallbackType callbackType, ScanResult scanResult)
+        {
+            base.OnScanResult(callbackType, scanResult);
+
+            Debug.WriteLine($"OnScanResult ({callbackType}):");
+
+            TraceScanResult(scanResult);
+        }
+
+        private void TraceScanResult(ScanResult scanResult)
+        {
+            Debug.WriteLine($"    {scanResult.Device.Address}, DeviceName:{scanResult.Device.Name}, ScanRecord:{scanResult.ScanRecord.DeviceName}");
         }
     }
 }
